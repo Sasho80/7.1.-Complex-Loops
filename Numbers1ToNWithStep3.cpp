12c171a2d7200dd4ba555354f59bea8b332bf6f0
Aleksandar <Sasho80@gbg.bg>
2023-08-27 21:14:44 +0300

    Update Numbers1ToNWithStep3.cpp

--- Numbers1ToNWithStep3.cpp
+++ Numbers1ToNWithStep3.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	//чрез i+=3 повишаване стойността на i с размера на стъпката
+	//by i += 3 increasing the value of i by the step size
 	for ( int i = 1; i <=n;  i+=3){
 		cout << i << endl;
 	}
