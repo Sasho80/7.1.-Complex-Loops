4b6248507331b31df840c190646b48e9899fc234
Aleksandar <Sasho80@gbg.bg>
2023-09-11 22:47:46 +0300

    Add files via upload

---
+++ NumberTable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	for (int row = 0; row < n; row++){
+		for (int col= 0; col < n; col++){
+			int num = row + col + 1;
+			if (num > n) {
+				num = 2 * n - num;
+			}
+			cout << num << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
